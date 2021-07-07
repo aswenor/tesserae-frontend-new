857df4793a0dc6ab9e067b82b4970c9ee925314c
Jeff Kinnison <jeffdkinnison@gmail.com>
2021-07-07 09:03:05 -0500

    rename downloads page component

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 import About from './components/about';
 import Corpus from './components/corpus';
-import Download from './components/download';
+import Downloads from './components/download';
 import Multitext from './components/multitext';
 import Reader from './components/reader';
 import Search from './components/search';
@@ -11,7 +11,7 @@ const routes = [
   {name: 'Corpus', url: '/corpus', component: Corpus, show: true},
   {name: 'Search', url: '/', component: Search, show: true},
   {name: 'Multitext', url: '/multitext', component: Multitext, show: true},
-  {name: 'Download', url: '/download', component: Download, show: true},
+  {name: 'Downloads', url: '/download', component: Downloads, show: true},
   {name: 'Reader', url: '/reader', component: Reader}
 ]
 
