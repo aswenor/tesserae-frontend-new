58870feda255eb57b7e7e6e764701537c5394c3f
aswenor <88151893+aswenor@users.noreply.github.com>
2023-01-12 12:58:04 -0500

    Fix issue with paragraphs

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -64,28 +64,28 @@ function About(props) {
           </Grid>
           <Grid item xs={12}>
             <Typography align="center">
-              Tesserae is a collaborative project of the <a href="https://www.buffalo.edu">University of Buffalo</a> 
+              <p> Tesserae is a collaborative project of the <a href="https://www.buffalo.edu">University of Buffalo</a> 
               <a href="https://arts-sciences.buffalo.edu/classics.html">Department of Classics</a> and
               <a href="https://arts-sciences.buffalo.edu/linguistics.html">Department of Linguistics</a>, the 
               <a href="https://cse.nd.edu/">Department of Computer Science and Engineering</a> of the 
               <a href="https://www.nd.edu/">University of Notre Dame</a>, and the 
               <a href="https://www.unige.ch/lettres/antic/">Département des Sciences de l\'Antiquité</a> of the 
               <a href="https://unige.ch/">University of Geneva</a>. 
-              <br>
+              </p>
                 
-              The principal investigators are <a href="https://arts-sciences.buffalo.edu/classics/faculty/core-faculty.host.html/content/shared/arts-sciences/classics/core-faculty/coffee-neil.html">Neil Coffee</a>,
+              <p> The principal investigators are <a href="https://arts-sciences.buffalo.edu/classics/faculty/core-faculty.host.html/content/shared/arts-sciences/classics/core-faculty/coffee-neil.html">Neil Coffee</a>,
               Professor of Classics, University of Buffalo; <a href="https://www.wjscheirer.com/">Walter J. Scheirer</a>, 
               Associate Professor of Computer Science, University of Notre Dame; and
               <a href="https://arts-sciences.buffalo.edu/linguistics/faculty.host.html/content/shared/arts-sciences/linguistics/new-faculty-profiles/koenig-jean-pierre.html">Jean-Pierre Koenig</a>,
               Professor of Linguistics, University of Buffalo. A list of personnel and collaborators is <a href="https://tesserae.caset.buffalo.edu/blog/people/">here</a>.
-              <br>
+              </p>
               
-              This project has been funded by the University of Buffalo College of Arts and Sciences, the National Endowment for the Humanities Office of Digital Humanities
+              <p> This project has been funded by the University of Buffalo College of Arts and Sciences, the National Endowment for the Humanities Office of Digital Humanities
               (<a href="https://securegrants.neh.gov/PublicQuery/main.aspx?f=1&gn=HD-51570-12">Start-up Grant #HD-51570</a>) and the Swiss National Science Foundation
               (<a href="https://data.snf.ch/grants/grant/146976">Project #146976</a>), and by the Digital Humanities Initiative at Buffalo. 
-              <br>
+              </p>
                 
-              For more information, contact Dr. Neil Coffee (<a href="ncoffee@buffalo.edu">ncoffee@buffalo.edu</a>) 
+              <p> For more information, contact Dr. Neil Coffee (<a href="ncoffee@buffalo.edu">ncoffee@buffalo.edu</a>) </p>
             </Typography>
           </Grid>
         </Grid>
