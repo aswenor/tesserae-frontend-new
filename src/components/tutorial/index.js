7444e5ec43e2698a9533b20827091f1560c2a024
aswenor <88151893+aswenor@users.noreply.github.com>
2023-05-24 12:42:48 -0400

    Fixing issues with updates with basic search

--- src/components/tutorial/index.js
+++ src/components/tutorial/index.js
@@ -67,7 +67,11 @@ function Tutorial(props) {
             <Typography align="left">
               The basic search compares two texts to find every place where they share two or more words within a single line or phrase. Shared words are those that have a common lemma or are considere semantically related, with the latter category including but not limited to synonyms and antonyms. 
             </Typography>
+          </Grid>
+          <Grid item xs={12}>
             <Typography align="left" variant='h3'>Target and Source</Typography>
+          </Grid>
+          <Grid item xs={12}>
             <Typography align="left"
               The “target” text is the text you are studying most closely. It is generally the alluding text, and the more recent. The search is bidirectional, so the choice of which text is the target and which the source has no influence on the search. In the display of results, however, phrases from the target text will appear in the left column, those from the source on the right. 
             </Typography>
