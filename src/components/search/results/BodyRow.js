5bce406893b7d25c00930b7c4705928ee19d8ae2
aswenor <88151893+aswenor@users.noreply.github.com>
2023-06-30 12:21:24 -0400

    Testing both target and source div matching

--- src/components/search/results/BodyRow.js
+++ src/components/search/results/BodyRow.js
@@ -93,7 +93,7 @@ function BodyRow(props) {
   
   // Check for subsection tags and if they match (or its a full text search) return the corresponding BodyRow object
   //if( (isNaN(sourceDiv) && isNaN(targetDiv)) || ((sourceDiv === sourceSection) && isNaN(targetDiv)) || (isNaN(sourceDiv) && (targetDiv === targetSection)) || ((sourceDiv === sourceSection) && (targetDiv === targetSection))) {
-  if ((targetDiv === targetSection) ) {  
+  if ((sourceDiv === sourceSection) && (targetDiv === targetSection)) {  
     return (
     <TableRow
       className={classes.row}
