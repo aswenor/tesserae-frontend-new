d98c2a655763a5d6a0c7ba142842d9a95b1cb72f
aswenor <88151893+aswenor@users.noreply.github.com>
2023-07-06 14:29:10 -0400

    Update index.js for default greek2latin search settings

--- src/components/greek-to-latin/index.js
+++ src/components/greek-to-latin/index.js
@@ -1,1 +1,25 @@
-// This is the base file for the greek-to-latin search 
+import React from 'react';
+
+import PageContainer from '../common/PageContainer';
+import RightPanel from './RightPanel';
+import Greek2LatinSearchParametersForm from './Greek2LatinSearchParametersForm';
+import TwoPanelView from '../common/TwoPanelView';
+
+
+function Search(props) {
+  return (
+    <PageContainer>
+      <TwoPanelView
+        leftMaxWidth={500}
+        leftMinWidth={300}
+        showToggle={true}
+      >
+        <Greek2LatinSearchParametersForm />
+        <RightPanel />
+      </TwoPanelView>
+    </PageContainer>
+  );
+}
+
+
+export default Greek_to_Latin;
