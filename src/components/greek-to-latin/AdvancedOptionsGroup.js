9615479e07bc9724d6e84488cc9b0b6d4c1ea59c
aswenor <88151893+aswenor@users.noreply.github.com>
2023-07-06 14:19:17 -0400

    Create default AdvancedOptionsGroup.js for greek2latin search

---
+++ src/components/greek-to-latin/AdvancedOptionsGroup.js
@@ -0,0 +1,61 @@
+/**
+ * @fileoverview Form for specifying advanced Tesserae search options.
+ * 
+ * @author [Jeff Kinnison](https://github.com/jeffkinnison)
+ * 
+ * @exports AdvancedOptionsGroup
+ * 
+ * @requires NPM:react
+ * @requires NPM:@mui/material
+ * @requires ../common/inputs/DistanceBasisInput
+ * @requires ../common/inputs/DropScoresInput
+ * @requires ../common/inputs/FeatureInput
+ * @requires ../common/inputs/FrequencyBasisInput
+ * @requires ../common/inputs/ScoreBasisInput
+ * @requires ../common/inputs/StoplistBasisInput
+ * @requires ../common/inputs/StoplistInput
+ * @requires ../common/inputs/UnitInput
+ */
+import React from 'react';
+
+import Box from '@mui/material/Box';
+
+import DistanceBasisInput from '../common/inputs/DistanceBasisInput';
+import DropScoresInput from '../common/inputs/DropScoresInput';
+import FeatureInput from '../common/inputs/FeatureInput';
+import FrequencyBasisInput from '../common/inputs/FrequencyBasisInput';
+import ScoreBasisInput from '../common/inputs/ScoreBasisInput';
+import StoplistBasisInput from '../common/inputs/StoplistBasisInput';
+import StoplistInput from '../common/inputs/StoplistInput';
+import UnitInput from '../common/inputs/UnitInput';
+
+
+/**
+ * Form to specify advanced Tesserae search options.
+ * 
+ * @component
+ * 
+ * @example
+ *  
+ *  return (
+ *    <AdvancedOptionsGroup />
+ *  );
+ */
+function AdvancedOptionsGroup(props) {
+  return (
+    <Box>
+        <UnitInput />
+        <FeatureInput />
+        <StoplistInput />
+        <StoplistBasisInput />
+        <FrequencyBasisInput />
+        <ScoreBasisInput />
+        <DistanceBasisInput />
+        <DropScoresInput />
+    </Box>
+  );
+}
+
+
+// Do redux binding here.
+export default AdvancedOptionsGroup;
