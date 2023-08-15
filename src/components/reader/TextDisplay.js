5ddf9ccd6387ca3c3116cabfabceba52d952cdbb
aswenor <88151893+aswenor@users.noreply.github.com>
2023-08-15 15:44:33 -0400

    Fixing reader scrolling

--- src/components/reader/TextDisplay.js
+++ src/components/reader/TextDisplay.js
@@ -13,7 +13,7 @@ const useStyles = makeStyles(theme => ({
   root: {
     height: '100%',
     // overflowX: 'hidden',
-    overflowY: 'scroll'
+    overflow: 'scroll'
   }
 }));
 
@@ -40,6 +40,7 @@ function TextDisplay(props) {
       className={classes.root}
       justify="center"
       spacing={2}
+      overflow="scroll"
     >
       {unitRows}
     </Grid>
