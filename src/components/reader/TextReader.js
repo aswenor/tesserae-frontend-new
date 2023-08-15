6885952ce0c74c06661315648aa801cc5ff51121
aswenor <88151893+aswenor@users.noreply.github.com>
2023-08-15 12:52:47 -0400

    Fix theme on reader

--- src/components/reader/TextReader.js
+++ src/components/reader/TextReader.js
@@ -23,7 +23,7 @@ const useStyles = makeStyles(theme => ({
     maxHeight: '100%',
   },
   paper: {
-    backgroundColor: theme.palette.secondary.main,
+    backgroundColor: theme.palette.secondary.light,
     display: 'block',
     height: '100%',
     paddingLeft: theme.spacing(2),
