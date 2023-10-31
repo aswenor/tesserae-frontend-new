9c4d38cce0bd4ca946b15e9fd9ca5f1ec64ffa44
aswenor <88151893+aswenor@users.noreply.github.com>
2023-10-31 11:56:59 -0400

    Debugging english text dropdowns

--- src/components/search/TextSelectGroup.js
+++ src/components/search/TextSelectGroup.js
@@ -181,7 +181,7 @@ const mapStateToProps = (state) => {
   return {
     availableSourceTexts: state.corpus.availableSourceTexts,
     availableTargetTexts: state.corpus.availableTargetTexts,
-    language: state.corpus.language,
+    language: 'english',//state.corpus.language,
     sourceDivision: state.search.sourceDivision,
     sourceText: state.search.sourceText,
     targetDivision: state.search.targetDivision,
